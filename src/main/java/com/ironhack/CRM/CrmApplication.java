17b6b8fe494ba238c533965ddaaa073592578e94
LauraAdom <laura.adomaityt@gmail.com>
2022-05-08 19:49:16 +0200

    convertLead modificated, typeOfContact and typeOfAccount created

--- src/main/java/com/ironhack/CRM/CrmApplication.java
+++ src/main/java/com/ironhack/CRM/CrmApplication.java
@@ -1,13 +1,24 @@
 package com.ironhack.CRM;
 
+import com.ironhack.CRM.CRM.CRM;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class CrmApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrmApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+
+		CRM crm = new CRM();
+		crm.menu(scanner);
+
+		scanner.close();
 	}
 
 }
