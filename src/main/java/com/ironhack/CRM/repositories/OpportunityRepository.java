f6b5c001b2df2beebb94103a67db49d7e27a8518
carvalen <carmenpilar1990@gmail.com>
2022-05-11 13:42:13 +0200

    Add country queries

--- src/main/java/com/ironhack/CRM/repositories/OpportunityRepository.java
+++ src/main/java/com/ironhack/CRM/repositories/OpportunityRepository.java
@@ -16,16 +16,46 @@ public interface OpportunityRepository extends JpaRepository<Opportunity, String
     List<Opportunity> findCountGroupByProduct();
 
     //“Report CLOSED-WON by the product”
-    @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'CLOSED_LOST' GROUP BY product", nativeQuery = true)
-    List<Opportunity> findCountWithStatusLostGroupByProduct();
-
-    // “Report CLOSED-LOST by the product”
     @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'CLOSED_WON' GROUP BY product", nativeQuery = true)
     List<Opportunity> findCountWithStatusWonGroupByProduct();
+
+    // “Report CLOSED-LOST by the product”
+    @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'CLOSED_LOST' GROUP BY product", nativeQuery = true)
+    List<Opportunity> findCountWithStatusLostGroupByProduct();
 
     //“Report OPEN by the product”
     @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'OPEN' GROUP BY product", nativeQuery = true)
     List<Opportunity> findCountWithStatusOpenGroupByProduct();
 
+    //By country:
+    //Report Opportunity by country
+    @Query(value = "SELECT COUNT(*) FROM opportunity GROUP BY country", nativeQuery = true)
+    List<Opportunity> findCountGroupByCountry();
+
+    //Report CLOSED-WON by the country
+    @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'CLOSED_WON' GROUP BY country", nativeQuery = true)
+    List<Opportunity> findCountWithStatusWonGroupByCountry();
+
+    //Report CLOSED-LOST by the country
+    @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'CLOSED_LOST' GROUP BY country", nativeQuery = true)
+    List<Opportunity> findCountWithStatusLostGroupByCountry();
+
+    //Report OPEN by country
+
+    @Query(value = "SELECT COUNT(*) FROM opportunity WHERE status = 'OPEN' GROUP BY country", nativeQuery = true)
+    List<Opportunity> findCountWithStatusOpenGroupByCountry();
+
+
+
+    //By Quantity:
+
+    // Mean quantity of products
+
+
+    // Median quantity of products
+
+    // Maximum quantity of products
+
+    // Minimum quantity of products
 
 }
