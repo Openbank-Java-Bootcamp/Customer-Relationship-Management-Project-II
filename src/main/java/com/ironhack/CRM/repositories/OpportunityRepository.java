f49d3bdeff7f76c4a13eea6f39a7718c8403755c
Carmen Valencia <63664372+carvalen@users.noreply.github.com>
2022-05-12 21:29:46 +0200

    Add Quantity queries (#14)

--- src/main/java/com/ironhack/CRM/repositories/OpportunityRepository.java
+++ src/main/java/com/ironhack/CRM/repositories/OpportunityRepository.java
@@ -88,14 +88,25 @@ public interface OpportunityRepository extends JpaRepository<Opportunity, String
 
     //By Quantity:
 
-    // Mean quantity of products
+    // The mean quantity of products order can be displayed by typing “Mean Quantity”
+    @Query(value = "SELECT AVG(quantity_of_product) FROM opportunity", nativeQuery = true)
+    double findMeanEmployeeCount();
 
-
-    // Median quantity of products
-
-    // Maximum quantity of products
-
-    // Minimum quantity of products
+    // The median quantity of products order can be displayed by typing “Median Quantity”
+    @Query(value = "SELECT AVG(aa.quantity_of_product) as median" +
+            " FROM (" +
+            "SELECT a.quantity_of_product, @rownum:=@rownum+1 as `row_number`, @total_rows:=@rownum" +
+            " FROM account a, (SELECT @rownum:=0) r" +
+            " WHERE a.quantity_of_product is NOT NULL" +
+            " ORDER BY a.quantity_of_product) as aa" +
+            " WHERE aa.row_number IN (FLOOR((@total_rows+1)/2), FLOOR((@total_rows+2)/2));", nativeQuery = true)
+    int findMedianQuantityProduct();
+    // The maximum quantity of products order can be displayed by typing “Max Quantity”
+    @Query(value = "SELECT MAX(quantity_of_product) FROM opportunity", nativeQuery = true)
+    int MaxQuantityOfProduct();
+    // The minimum quantity of products order can be displayed by typing “Min Quantity”
+    @Query(value = "SELECT MIN(quantity_of_product) FROM opportunity", nativeQuery = true)
+    int MinQuantityOfProduct();
 
 
     //“Report Opportunity by City”
